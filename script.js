f62e529c7f43963c208e5036edb25d151358d978
w4lt3r4l4rc0n <walarcon@mdconsgroup.com>
2026-03-09 00:01:12 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function reunirEsferas(){
+
+document.getElementById("dragonSound").play();
+
+document.querySelector(".container").style.display="none";
+
+setTimeout(()=>{ 
+document.getElementById("shenlongContainer").style.display="block";
+},2000);
+
+}
+
+function mostrarInvitacion(){
+
+document.getElementById("shenlongContainer").style.display="none";
+
+document.getElementById("invitacion").style.display="block";
+
+}
+
+function aceptar(){
+
+alert("¡Excelente guerrero! Nos vemos en la boda ⚡");
+
+}
+
+function rechazar(){
+
+alert("Shenlong dice: lo reconsiderarás 😆");
+
+}
